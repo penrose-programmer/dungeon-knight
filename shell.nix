ec8344eeaa62db2795aba8e12acc52b984773835
penrose-programmer <syedtalhahahmed@gmail.com>
2025-09-09 06:37:33 +1000

    Hello, World!

--- shell.nix
+++ shell.nix
@@ -3,8 +3,5 @@ with pkgs;
 mkShell {
   buildInputs = [
     cargo
-    pkgconfig udev alsaLib lutris
-    x11 xorg.libXcursor xorg.libXrandr xorg.libXi
-    vulkan-tools vulkan-headers vulkan-loader vulkan-validation-layers
   ];
 }
